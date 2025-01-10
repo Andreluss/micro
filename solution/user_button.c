7689a56a1987db27857ab809888348f04bd5e49f
Andrzej Jablonski <aj448257@students.mimuw.edu.pl>
2025-01-10 11:57:33 +0100

    button press handler

--- solution/user_button.c
+++ solution/user_button.c
@@ -1,7 +1,7 @@
 #include "user_button.h"
 
 #define USER_BTN_GPIO GPIOC
-#define USER_BTN_PIN  13
+#define USER_BTN_PIN 13
 
 #define ACTION_BTN_GPIO GPIOB
 #define ACTION_BTN_PIN 10
@@ -23,6 +23,7 @@ static void user_button_invoke_on_pressed()
 void user_button_init(void (*on_user_button_pressed_)())
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; // enable GPIOs of the button
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; // for button interrupts
     __NOP(); __NOP();
 
     user_button_set_on_pressed(on_user_button_pressed_);
@@ -47,10 +48,9 @@ static bool is_pressed(uint32_t pin_number, GPIO_TypeDef *gpio) {
 static void exti_handle_button(uint32_t btn_pin, GPIO_TypeDef *gpio, const char* name, bool signal_pressed) {
     if (EXTI->PR & (1 << btn_pin)) {
         EXTI->PR = (1 << btn_pin); // setting the bit clears the interrupt
-        user_button_invoke_on_pressed();
-        // if (is_pressed(btn_pin, gpio) == signal_pressed) {
-        //     user_button_invoke_on_pressed();
-        // }
+        if (is_pressed(btn_pin, gpio) == signal_pressed) {
+            user_button_invoke_on_pressed();
+        }
     }
 }
 
