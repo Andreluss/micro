50dafbfad43588e42fa0f2a5038e5eb287eb7d2b
Andrzej Jablonski <aj448257@students.mimuw.edu.pl>
2025-01-21 01:00:07 +0100

    wip

--- solution/usart.h
+++ solution/usart.h
@@ -4,5 +4,3 @@
 void usart_init(int baudrate, int pclk_hz);
 // Returns != 0 if send is incomplete
 int usart_send_byte(uint8_t byte);
-// Returns != 0 if send is incomplete
-int usart_send_string(const char *string);
